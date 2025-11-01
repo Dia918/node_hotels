d258036f718d54761ef50722aac5f10a30024232
Dia Negi <negidia10@gmail.com>
2025-11-02 02:03:17 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res)=>{ 
     res.send("hello how can i help you");
@@ -21,6 +23,7 @@ const menuRoutes = require('./routes/menuRoutes');
 //use the routes of menu
 app.use('/menuItems', menuRoutes)
 
-app.listen(3001, ()=>{
+
+app.listen(PORT, ()=>{
     console.log('listening on port 3001');
 })
