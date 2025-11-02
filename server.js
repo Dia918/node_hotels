6c889eae4ee329d51c7f7694f55f6077494136f2
Dia Negi <negidia10@gmail.com>
2025-11-02 23:43:56 +0530

    adding authentification

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Render server is working!');
-});
+//Middleware function
+const logRequest = (req, res, next) =>{
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); //move to the next phase
+}
+
+app.use(logRequest);
+
+app.use(passport.initialize());
+
+const localAuthMiddleware = passport.authenticate('local', {session: false}) 
 
 app.get('/', (req, res)=>{ 
     res.send("hello how can i help you");
@@ -19,13 +28,13 @@ app.get('/', (req, res)=>{
 const personRoutes = require('./routes/personRoutes');
 
 //use the routes of person
-app.use('/person', personRoutes);
+app.use('/person', localAuthMiddleware,  personRoutes);
 
 //import the router files of menu
 const menuRoutes = require('./routes/menuRoutes');
 
 //use the routes of menu
-app.use('/menuItems', menuRoutes)
+app.use('/menuItems',  menuRoutes)
 
 
 app.listen(PORT, '0.0.0.0', ()=>{
